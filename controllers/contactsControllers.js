04970e8b1f92890f4d91f59805df47ee06a4cd1b
philipps777 <7philipps7@gmail.com>
2025-02-20 12:47:06 +0200

    commin node-rest-api

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,72 @@
+
+import {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContactById
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+
+
+export const getAllContacts = async (req, res, next) => {
+    try {
+        const contacts = await listContacts();
+        res.status(200).json(contacts);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getOneContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const contact = await getContactById(id);
+        if (!contact) {
+            throw HttpError(404);
+        }
+        res.status(200).json(contact);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const contact = await removeContact(id);
+        if (!contact) {
+            throw HttpError(404);
+        }
+        res.status(200).json(contact);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export const createContact = async (req, res, next) => {
+    try {
+        const contact = await addContact(req.body);
+        res.status(201).json(contact);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { body } = req;
+        if (Object.keys(body).length === 0) {
+            throw HttpError(400, "Body must have at least one field");
+        }
+        const updatedContact = await updateContactById(id, body);
+        if (!updatedContact) {
+            throw HttpError(404);
+        }
+        res.status(200).json(updatedContact);
+    } catch (error) {
+        next(error);
+    }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,47 @@
+import { nanoid } from "nanoid";
+import { readFile, writeFile } from "node:fs/promises";
+import { resolve } from "node:path";
+
+
+const contactsPath = resolve("db", "contacts.json");
+
+export async function listContacts() {
+    const data = await readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+}
+
+export async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact || null;
+}
+
+export async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [removedContact] = contacts.splice(index, 1);
+    await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+}
+
+export async function addContact({ name, email, phone }) {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), name, email, phone };
+    contacts.push(newContact);
+    await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+}
+
+export async function updateContactById(contactId, data) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    contacts[index] = { ...contacts[index], ...data };
+    await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[index];
+}
