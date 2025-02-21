f5e021e83a93f9ce2008d5eaef6409e21b9eb0b4
philipps777 <7philipps7@gmail.com>
2025-02-21 16:12:13 +0200

    fix ContactSchemas

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,13 +1,22 @@
 import Joi from "joi";
 
+const requiredFields = ["name", "email", "phone"];
+
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().pattern(/^[0-9]{10}$/).required(),
+    phone: Joi.string().required(),
 });
 
+
 export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(30),
+    name: Joi.string(),
     email: Joi.string().email(),
-    phone: Joi.string().pattern(/^[0-9]{10}$/),
-}).min(1);
+    phone: Joi.string(),
+  })
+    .or(...requiredFields)
+    .messages({
+      "object.missing": `Body must have at least one field [${requiredFields.join(
+        ", "
+      )}]`,
+    });
