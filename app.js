fba5c82fc43dec92e3d82653c82b6a49e36f1305
philipps777 <7philipps7@gmail.com>
2025-03-02 20:14:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,9 +27,7 @@ app.use((err, req, res, next) => {
 });
 
 
-// app.listen(3000, () => {
-//   console.log("Server is running. Use our API on port: 3000");
-// });
+
 
 const startServer = async () => {
   await connectDB();
