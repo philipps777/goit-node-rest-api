716681d3bb9ab06820aa1c8b1034d81523a19fdb
philipps777 <7philipps7@gmail.com>
2025-03-11 22:24:55 +0200

    add avatar, updateavatar

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,6 +3,7 @@ import validateBody from "../helpers/validateBody.js";
 import { authSchema } from "../schemas/authSchemas.js";
 import { register, login, logout, getCurrentUser } from "../controllers/authController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { updateAvatar, upload } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
@@ -10,6 +11,7 @@ authRouter.post("/register", validateBody(authSchema), register);
 authRouter.post("/login", validateBody(authSchema), login);
 authRouter.post("/logout", authMiddleware, logout);
 authRouter.get("/current", authMiddleware, getCurrentUser);
+authRouter.patch("/avatars", authMiddleware, upload.single("avatar"), updateAvatar);
 
 
 export default authRouter;
